9d509be9ed7678b4b9b9a52aececd2cbd3c7b286
Jorge Luis <jorgestudio2017@gmail.com>
2021-05-20 15:32:50 -0400

    fix: binnacles table

--- app/DataTables/BinnaclesTable.php
+++ app/DataTables/BinnaclesTable.php
@@ -3,9 +3,15 @@
 namespace App\DataTables;
 
 use App\Models\Activity;
+use Illuminate\Database\Query\Builder;
 
-class BinnaclesTable extends Datatable
+class BinnaclesTable extends DataTable
 {
+    /**
+     * The query builder object
+     *
+     * @return Builder
+    */
     public function query()
     {
         return Activity::all();
